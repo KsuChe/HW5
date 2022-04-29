a4a839bf8ec2fc17c087f3b57f0fbf2d69a0a6b6
KsuChe <nerro@mail.ru>
2022-04-29 20:30:39 +0300

    task3

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,26 @@ public class Main {
         System.out.print(number[0] + "," + number[1]);
 
         System.out.println();
+        //task 3
+
+        weights[0] = 3;
+        weights[1] = 2;
+        weights[2] = 1;
+        System.out.print(weights[0] + "," + weights[1] + "," + weights[2]);
+
+        System.out.println();
+
+        weight[0] = 9.986f;
+        weight[1] = 7.654f;
+        weight[2] = 1.57f;
+        System.out.print(weight[0] + "," + weight[1] + "," + weight[2]);
+        System.out.println();
+
+        number[0] = 2;
+        number[1] = 1;
+        System.out.print(number[0] + "," + number[1]);
+        System.out.println();
+
 
     }
 
