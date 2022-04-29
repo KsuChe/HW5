bd69f73cd466f872964e1165ee70ee4bfeb81e5c
KsuChe <nerro@mail.ru>
2022-04-29 20:29:53 +0300

    task1,2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println();
+//task 1,2
+        int[] weights = new int[3];
+        weights[0] = 1;
+        weights[1] = 2;
+        weights[2] = 3;
+        System.out.print(weights[0] + "," + weights[1] + "," + weights[2]);
+
+        System.out.println();
+        float[] weight = {1.57f, 7.654f, 9.986f};
+        weight[0] = 1.57f;
+        weight[1] = 7.654f;
+        weight[2] = 9.986f;
+        System.out.print(weight[0] + "," + weight[1] + "," + weight[2]);
+
+        System.out.println();
+        long[] number = new long[2];
+        number[0] = 1;
+        number[1] = 2;
+
+        System.out.print(number[0] + "," + number[1]);
+
+        System.out.println();
+
     }
 
 }
